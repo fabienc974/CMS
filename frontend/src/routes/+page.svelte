1d562dfd63acfbdebca6d5f4802776c55cb7e086
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-04-11 17:43:35 +0200

    ajout fonction addTocart removeFromcart
    c'est soit pour rajouter ou retirer des cart

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script>
+	let quantity = 0;
+	function addTocart() {
+		inventory = [...inventory, quantity];
+		quantity = ++quantity;
+	}
+	function removeFromcart() {
+			quantity = --quantity;
+			inventory = inventory.slice(0,-1)
+		}
+	}
+	let inventory = [1, 2, 3];
+	console.log(inventory);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Your pizza cart has {quantity} items. UwU baka</h1>
+<button on:click={addTocart}> Add To Cart </button>
+<button on:click={removeFromcart}> Remove From Cart </button>
+<h2>{inventory}</h2>
