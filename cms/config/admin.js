0b95c1a68a5be93d93d6902cabc00bca566086ee
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-04-20 11:47:18 +0200

    ajout salt key

--- cms/config/admin.js
+++ cms/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET',['a5d5dfabb9086d13fc10e933fffcdd86']),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT',['9a75601ca5560f64d5d92bc0cb6a55c32308f5d0']),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT',['4f698f15e554a1674ce00f1026a161b4']),
     },
   },
   flags: {
--- cms/config/server.js
+++ cms/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['94f9963a5c501a1243091736eaf9eae73a590354']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
