5045e6f922ffb98c10ce3524da15edb9b3c22a74
fabienc974 <37477925+fabienc974@users.noreply.github.com>
2024-04-20 12:13:45 +0200

    changer key secrets

--- cms/config/admin.js
+++ cms/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET',['a5d5dfabb9086d13fc10e933fffcdd86']),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT',['9a75601ca5560f64d5d92bc0cb6a55c32308f5d0']),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT',['4f698f15e554a1674ce00f1026a161b4']),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
