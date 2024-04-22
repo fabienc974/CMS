a5afc8836654521e2e855f4869ce0ce1e605a954
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-04-22 18:21:16 +0200

    gridtitle

---
+++ frontend/src/lib/Components/GridTitle.svelte
@@ -0,0 +1,8 @@
+<slot/>
+<script>
+    // import Index from '../routes/index.svelte';
+</script>
+
+<div class="text-white bg-indigo-600 w-full h-full overflow-hidden">
+    <button class="relative w-full h-full flex items-center justify-center"></button>
+</div>
