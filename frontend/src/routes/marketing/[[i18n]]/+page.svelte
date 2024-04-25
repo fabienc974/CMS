726c78bfcaccfdadd18b4b431436d99c9a95f838
fabienc974 <37477925+fabienc974@users.noreply.github.com>
2024-04-25 08:41:48 +0200

    ajout d'un menu de test

---
+++ frontend/src/routes/marketing/[[i18n]]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+
+	const greetings = {
+		en: 'Hello !',
+		fr: 'Bonjour !',
+		es: 'Hola !'
+	};
+
+	const { i18n = 'fr' } = $page.params;
+	const greeting = greetings[i18n];
+</script>
+
+<p>{greeting}</p>
---
+++ frontend/src/routes/products/[categorySlug]/[productSlug]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+	// const reviewId = $page.params.reviewSlug;
+	// const productSlug = $page.params.productSlug;
+	// const categorySlug = $page.params.categorySlug;
+	const { productSlug, categorySlug, reviewId } = $page.params;
+</script>
+
+<p>Voir le review {reviewId} du produit {productSlug} de la catégorie {categorySlug}</p>
