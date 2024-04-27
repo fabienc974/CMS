c5611a1b61957e5af6386d22688e1e5c4ecad39d
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-04-27 11:56:31 +0200

    merge

---
+++ frontend/src/routes/nathan/product/[productID]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores'
+    const productID = $page.productID;
+</script>
+
+
+<h1>détails produit {productID}</h1>
---
+++ frontend/src/routes/nathan/product/[productID]/review/reviewID/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+    const { productID, reviewID } = $page.params;
+</script>
+
+<h1> Review {reviewID} for product {productID}</h1>
