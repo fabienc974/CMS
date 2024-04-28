78ecfd8219dff0dd2a748dd95eb428111ab77e19
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-04-28 14:40:08 +0200

    ttttt

---
+++ frontend/src/routes/nathan/[lang]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/stores';
+    const greetings = {
+        en: 'hello',
+        de: 'hallo',
+        fr: 'bonjour'
+    };
+    const { lang } = $page.params;
+    const greeting = greetings [lang];
+</script>
+
+<h1>{greeting}</h1>
