b861604adb42d22cf4f86b2d600693fc403eb278
fabienc974 <37477925+fabienc974@users.noreply.github.com>
2024-04-29 08:24:41 +0200

    Tuto 11 - navigation dynamique
    utiliser la fonction goto pour naviguer via du script

--- frontend/src/routes/products/+page.svelte
+++ frontend/src/routes/products/+page.svelte
@@ -1,6 +1,23 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+	const handleClick = () => {
+		console.log('placing an order');
+		goto('/test');
+	};
+	beforeNavigate((e) => console.log('before called',e));
+	afterNavigate((e) => console.log('after called',e));
+</script>
+
 <p class="text-sm">Ici on liste toutes les catégories de produits qu'on a</p>
 <ul class="w-full text-indigo-400">
-  <li>Pizza sauce tomate</li>
-  <li>Pizza crème frâiche</li>
-  <li>etc...</li>
+	<li>Pizza sauce tomate</li>
+	<li>Pizza crème frâiche</li>
+	<li>etc...</li>
 </ul>
+<div class="w-full">
+	<button
+		on:click={handleClick}
+		class="rounded-lg px-4 py-2 border-2 border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-blue-100 duration-300"
+		>Place order</button
+	>
+</div>
