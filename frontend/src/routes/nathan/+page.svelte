8a7e541156fbaa24eae356ece1cc381920dd727f
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-05-01 11:37:36 +0200

    f

--- frontend/src/routes/nathan/+page.svelte
+++ frontend/src/routes/nathan/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import { goto } from 
+    const handleClick  = () => {
+        console.log ('Placing your order');
+    };
+</script>
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+
+
+<button on:click={handleClick}>Place older</button>
--- frontend/src/routes/nathan/product/+page.svelte
+++ frontend/src/routes/nathan/product/+page.svelte
@@ -1,4 +1,7 @@
-<h1>product list</h1>
-<h2>product 1</h2>
-<h2>product 2</h2>
-<h2>product 3</h2>
+ <a href="/">Home</a>
+ <h1>product list</h1>
+
+<h2><a href="/products/1"> product 1</a> </h2>
+<h2><a href="/products/1"> product 2</a> </h2>
+<h2><a href="/products/1"> product 3</a> </h2>
+<h2><a href={`/products/${productID}`}>Product {productID}</a></h2>
