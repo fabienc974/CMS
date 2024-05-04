1c9d8e4695adb7abd6beee92121ea614d48b92a3
Evil974 <121492587+Evil974@users.noreply.github.com>
2024-05-04 11:52:18 +0200

    header footer
    j'ai fais ca

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -9,3 +9,24 @@
 	<slot />
 </main>
 <Footer />
+
+<header class="layout-header">Header</header>
+<footer class="layout-footer">Footer</footer>
+
+<style>
+	.layout-header {
+		background-color: #264653;
+		color: orange;
+		font-size:30px;
+		text-align: center;
+		padding: 10px;
+	}
+
+	.layout-footer{
+		background-color: #264653;
+		color: orange;
+		font-size:30px;
+		text-align: center;
+		padding: 10px;
+	}
+</style>
